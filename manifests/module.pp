e0de60204c64086f325dcd7674ab7bbf1b80c658
Fabian Dammekens <fabian.dammekens@cegeka.be>
2021-04-15 13:40:57 +0200

    [DNF] Module definition files should match the module name

--- manifests/module.pp
+++ manifests/module.pp
@@ -1,5 +1,10 @@
 # Define: dnf module
 #
+# A DNF module can be defined as followed:
+# 'module-stream':
+#   dnf_name: 'module'    # The official name of the DNF module (dnf module list)
+#   stream: 'stream'      # The stream of the DNF module (dnf module list)
+#
 define dnf::module (
   String $ensure,
   String $state='enabled',
@@ -8,22 +13,30 @@ define dnf::module (
   String $profiles='*'
 ) {
 
-  file { "/etc/dnf/modules.d/${title}.module":
-    ensure => $ensure
+  file { "/etc/dnf/modules.d/${dnf_name}.module":
+    ensure  => $ensure
+  }
+
+  file_line { "Dnf::Module ${dnf_name} owned by puppet":
+    path => "/etc/dnf/modules.d/${dnf_name}.module",
+    line => '# This file is managed by puppet'
   }
 
   if $ensure == present {
-    augeas { "/etc/dnf/modules.d/${title}.module":
+    augeas { "Dnf::Module ${title}":
       lens    => 'PHP.lns',
-      incl    => "/etc/dnf/modules.d/${title}.module",
-      context => "/files/etc/dnf/modules.d/${title}.module",
+      incl    => "/etc/dnf/modules.d/${dnf_name}.module",
+      context => "/files/etc/dnf/modules.d/${dnf_name}.module",
       changes => [
         "set ${dnf_name}/name '${dnf_name}'",
         "set ${dnf_name}/state '${state}'",
         "set ${dnf_name}/stream '${stream}'",
         "set ${dnf_name}/profiles '${profiles}'",
       ],
-      require => File["/etc/dnf/modules.d/${title}.module"]
+      require => [
+        File["/etc/dnf/modules.d/${dnf_name}.module"],
+        File_line["Dnf::Module ${dnf_name} owned by puppet"]
+      ]
     }
   }
 }
